b47168f30724fe1529e1d87afb012d8b4f19bae1
Alexandre Bordiere <alexandrebordiere@gmail.com>
2024-10-29 10:13:05 +1100

    Enable deletion protection & PITR for the DynamoDB table

--- dynamo.tf
+++ dynamo.tf
@@ -1,16 +1,21 @@
 resource "aws_dynamodb_table" "authentication" {
-  name           = var.dynamo_table_name
-  billing_mode   = "PROVISIONED"
-  read_capacity  = 20
-  write_capacity = 20
-  hash_key       = "UserId"
+  name                        = var.dynamo_table_name
+  billing_mode                = "PROVISIONED"
+  deletion_protection_enabled = true
+  hash_key                    = "UserId"
+  read_capacity               = 20
+  write_capacity              = 20
 
   attribute {
     name = "UserId"
     type = "S"
   }
 
+  point_in_time_recovery {
+    enabled = true
+  }
+
   tags = {
-    Name = "${var.dynamo_table_name}"
+    Name = var.dynamo_table_name
   }
 }
